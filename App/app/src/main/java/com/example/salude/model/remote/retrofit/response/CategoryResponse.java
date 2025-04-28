eacd9cc06b972008fc4f8fe91c160a07d1b0b0bf
AhmedAlyElGhannam <ahmedaly.g112@gmail.com>
2025-04-28 18:31:44 +0300

    completed the rest of responses pojo

--- App/app/src/main/java/com/example/salude/model/remote/retrofit/response/CategoryResponse.java
+++ App/app/src/main/java/com/example/salude/model/remote/retrofit/response/CategoryResponse.java
@@ -1,4 +1,14 @@
 package com.example.salude.model.remote.retrofit.response;
 
+import com.example.salude.model.pojo.Category;
+import com.example.salude.model.pojo.FilteredMeal;
+
+import java.util.List;
+
 public class CategoryResponse {
+    private List<Category> categories;
+
+    public List<Category> getCategories() {
+        return categories;
+    }
 }
--- App/app/src/main/java/com/example/salude/model/remote/retrofit/response/FilteredMealResponse.java
+++ App/app/src/main/java/com/example/salude/model/remote/retrofit/response/FilteredMealResponse.java
@@ -1,4 +1,13 @@
 package com.example.salude.model.remote.retrofit.response;
 
+import com.example.salude.model.pojo.FilteredMeal;
+
+import java.util.List;
+
 public class FilteredMealResponse {
+    private List<FilteredMeal> filteredMeals;
+
+    public List<FilteredMeal> getFilteredMeals() {
+        return filteredMeals;
+    }
 }
